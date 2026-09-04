eeb438f23c47d078dddd28206e4e53ddc17553db
Gaurav_Malvi <gouravmalvi18@gmail.com>
2026-09-04 12:59:22 +0530

    Deploy : Config the project for vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import app from "./src/app.js";
 import connectDB from "./src/Configuration/DB.config.js";
 import config from "./src/Configuration/Env.config.js";
-const port = config.PORT || 8000;
 
 //DateBase Connection
 connectDB();
 
-app.listen(port, () => {
-  console.log("Server Up !!!");
-});
+if (process.env.NODE_ENV !== "production") {
+  const port = config.PORT || 8000;
+  app.listen(port, () => {
+    console.log("Server Up !!!");
+  });
+}
+
+export default app;
--- backend/src/app.js
+++ backend/src/app.js
@@ -7,14 +7,40 @@ import cors from "cors";
 const app = express();
 
 //middleware
+// app.use(     // for normal and local development this will work 
+//   cors({
+//     origin: process.env.CLIENT_URL,  
+//     credentials: true,
+//   }),
+// );
+
+// for Deployment this need to be used
+
+// CORS Configuration
+// CORS — allows only specific origins
+const allowedOrigins = [
+  process.env.LOCAL_CLIENT_URL, // frontend localhost URL
+  process.env.DEPLOY_CLIENT_URL, // frontend deployment URL
+].filter(Boolean); // removes undefined if DEPLOY_CLIENT_URL not set yet
+
+const corsOptions = {                      
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error("Not allowed by CORS"));
+  },
+  credentials: true,
+};
+
+app.options(/.*/, cors(corsOptions)); // handle preflight requests 
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  }),
-);
 
 /**@import routes */
 import AuthRouter from "./feature/AuthFeature/routes/Auth.route.js";
